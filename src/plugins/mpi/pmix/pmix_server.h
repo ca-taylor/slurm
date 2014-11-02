582f6fe8e7736c432c89174bb75c5de4d4589558
Artem Polyakov <artpol84@gmail.com>
2014-11-02 12:03:02 +0600

    First working version of PMI-like SLURM plugin
    1. Implement Collectives
    2. Implement simplified PMI-like scenario (hellompi.c) when:
    - app process registers at pmi server
    - gets some useful info about environment (gets it's global rank and number of tasks)
    - starts the fence
    - receives blobs from all tasks

--- src/plugins/mpi/pmix/pmix_server.h
+++ src/plugins/mpi/pmix/pmix_server.h
@@ -3,7 +3,12 @@
 
 #include "pmix_common.h"
 
-
-void pmix_server_request(eio_handle_t *h, int fd);
+int pmix_stepd_init(const stepd_step_rec_t *job, char ***env);
+int pmix_srun_init(const mpi_plugin_client_info_t *job, char ***env);
+void pmix_server_request(int fd);
+char *pmix_server_alloc_msg(uint32_t size, char **payload);
+void pmix_server_msg_set_fence(char *msg);
+void pmix_server_msg_set_fence_resp(char *msg);
+uint32_t pmix_server_sendmsg_size(char *msg);
 
 #endif // SERVER_H
