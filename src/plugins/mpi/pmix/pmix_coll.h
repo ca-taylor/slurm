6d54aefbad04d4530854f400544fb6aa59e31af7
Artem Polyakov <artpol84@gmail.com>
2014-11-02 20:29:54 +0600

    Fix async IO: handling of closed descriptors.

--- src/plugins/mpi/pmix/pmix_coll.h
+++ src/plugins/mpi/pmix/pmix_coll.h
@@ -5,5 +5,6 @@
 int pmix_coll_init(char ***env);
 void pmix_coll_node_contrib(uint32_t nodeid, void *msg, uint32_t size);
 void pmix_coll_task_contrib(uint32_t taskid, void *msg, uint32_t size);
+void pmix_coll_update_db(void *msg, uint32_t size);
 
 #endif // PMIX_COLL_H
