8dd462b782cb044d752c248a50cc6a62d3111ab6
Artem Polyakov <artpol84@gmail.com>
2015-04-11 11:40:40 +0600

    Fix PMIx plugin version number

--- src/plugins/mpi/pmix/mpi_pmix.c
+++ src/plugins/mpi/pmix/mpi_pmix.c
@@ -81,7 +81,7 @@
  */
 const char plugin_name[]        = "PMIx plugin";
 const char plugin_type[]        = "mpi/pmix";
-const uint32_t plugin_version   = 100;
+const uint32_t plugin_version   = SLURM_VERSION_NUMBER;
 
 int p_mpi_hook_slurmstepd_prefork(const stepd_step_rec_t *job, char ***env)
 {
